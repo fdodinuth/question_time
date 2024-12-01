02097a3a949fc6db838bd4d290aa003b787a4084
fdodinuth <fdodinuth@gmail.com>
2024-12-01 11:08:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,14 @@
-from flask import Flask, render_template, request, jsonify, redirect, url_for, session
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
-
-# Secret key for session management
-app.secret_key = 'your_secret_key_here'
 
 # Global variable to store table data
 table_data = []
 
-# Route for the login page
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-
-        # Check if username and password are correct
-        if username == 'admin' and password == 'dinuth':
-            session['logged_in'] = True
-            return redirect(url_for('presenter_page'))
-        else:
-            return render_template('login_page.html', error='Invalid credentials')
-    return render_template('login_page.html')
-
-# Route for the presenter page (protected by login)
+# Route for the presenter page
 @app.route('/')
 def presenter_page():
-    if 'logged_in' in session:
-        return render_template('presenter_page.html')
-    else:
-        return redirect(url_for('login'))
+    return render_template('presenter_page.html')
 
 # Route to retrieve table data for the presenter page
 @app.route('/get_table_numbers', methods=['GET'])
@@ -54,12 +33,6 @@ def add_table_data():
 @app.route('/button')
 def button_page():
     return render_template('button_page.html')
-
-# Route to logout and end session
-@app.route('/logout')
-def logout():
-    session.pop('logged_in', None)  # Remove 'logged_in' session
-    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
